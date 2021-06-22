dca81276d52663ca4d81ff7987d3406c3ace70de
m05e5 <calebmoses1111@gmail.com>
2021-06-22 10:29:20 +0100

    Add event listener to js that remove toggle menu when click on nav items

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* eslint-disable linebreak-style */
 const menuToggle = document.querySelector('.menu-toggle input');
+const menulink = document.querySelector('nav ul li a');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('slide');
+});
+menulink.addEventListener('click', ()=>{
+  nav.classList.remove('slide');
 });
