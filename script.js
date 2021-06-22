ee3d21640ae7fe2ddd4b314f98859c1ec839cf12
NJ-2020 <ntt.london@gmail.com>
2021-06-22 14:59:10 +0700

    Add js for navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('slide');
+});
