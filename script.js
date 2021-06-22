1722255dda25f9de2a415425aad467ea3a4e24cf
NJ-2020 <ntt.london@gmail.com>
2021-06-22 21:44:53 +0700

    Add js code for popup window

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* eslint-disable linebreak-style */
+/* eslint-disable no-use-before-define *//* eslint-disable linebreak-style */
 const menuToggle = document.querySelector('.menu-toggle input');
 const menulink = document.querySelectorAll('nav ul li a');
 const nav = document.querySelector('nav ul');
@@ -9,3 +9,40 @@ menulink.forEach((link) => link.addEventListener('click', () => {
   nav.classList.remove('slide');
   document.querySelector('.menu-toggle input').checked = false;
 }));
+
+const openModalButton = document.querySelectorAll('[data-modal-target]');
+const closeModalButton = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelector('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
