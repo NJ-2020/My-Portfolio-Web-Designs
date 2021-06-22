1bc8a9cd8e336d06007f5132088b09a444684dcb
m05e5 <calebmoses1111@gmail.com>
2021-06-22 10:49:54 +0100

    added event listener to all the links in the nav menu

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /* eslint-disable linebreak-style */
 const menuToggle = document.querySelector('.menu-toggle input');
-const menulink = document.querySelector('nav ul li a');
+const menulink = document.querySelectorAll('nav ul li a');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('slide');
 });
-menulink.addEventListener('click', ()=>{
-  nav.classList.remove('slide');
-});
+menulink.forEach((link) => link.addEventListener('click',()=>  nav.classList.toggle('slide')));
