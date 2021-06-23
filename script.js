be5563ff2e353b9e32104f9f6cf334bc55414149
NJ-2020 <ntt.london@gmail.com>
2021-06-23 17:19:49 +0700

    Fix linter error

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-/* eslint-disable no-use-before-define *//* eslint-disable linebreak-style */
+/* eslint-disable no-use-before-define */
+/* eslint linebreak-style: ["error", "windows"] */
 const menuToggle = document.querySelector('.menu-toggle input');
 const menulink = document.querySelectorAll('nav ul li a');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('slide');
 });
+
 menulink.forEach((link) => link.addEventListener('click', () => {
   nav.classList.remove('slide');
   document.querySelector('.menu-toggle input').checked = false;
