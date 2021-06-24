a684af509b37fb21a555af19167f2d41a5fd4f45
NJ-2020 <ntt.london@gmail.com>
2021-06-24 21:33:50 +0700

    Fix linter error

--- script.js
+++ script.js
@@ -2,74 +2,74 @@ const menuToggle = document.querySelector('.menu-toggle input');
 const menulink = document.querySelectorAll('nav ul li a');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('slide');
+  nav.classList.toggle('slide');
 });
 
 menulink.forEach((link) => link.addEventListener('click', () => {
-    nav.classList.remove('slide');
-    document.querySelector('.menu-toggle input').checked = false;
+  nav.classList.remove('slide');
+  document.querySelector('.menu-toggle input').checked = false;
 }));
 // ------------- Portfolio Section ---------------
 const projects = [{
-        id: 0,
-        name: 'Tonic',
-        companyName: 'CANOPY',
-        position: 'Back End Dev',
-        year: 2015,
-        description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-        technologies: ['html', 'css', 'javascript'],
-        image: {
-            link: 'img/project0.png',
-            alt: 'Tonic screenshot',
-        },
-        liveSource: '#',
-        sourceCode: '#',
-    },
-    {
-        id: 1,
-        name: 'Multi-Post Stories',
-        companyName: 'FACEEBOOK',
-        position: 'Full Stack Dev',
-        year: 2015,
-        description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-        technologies: ['html', 'Ruby on Rails', 'css', 'javascript'],
-        image: {
-            link: 'img/project1.png',
-            alt: 'Multi-Post Stories screenshot',
-        },
-        liveSource: '#',
-        sourceCode: '#',
-    },
-    {
-        id: 2,
-        name: 'Facebook 360',
-        companyName: 'FACEEBOOK',
-        position: 'Full Stack Dev',
-        year: 2015,
-        description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-        technologies: ['html', 'css', 'javascript'],
-        image: {
-            link: 'img/project2.svg',
-            alt: 'Facebook 360screenshot',
-        },
-        liveSource: '#',
-        sourceCode: '#',
-    },
-    {
-        id: 3,
-        name: 'Uber Navigation',
-        companyName: 'Uber',
-        position: 'Lead Developer',
-        year: 2018,
-        description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-        technologies: ['html', 'Ruby on Rails', 'css', 'javascript'],
-        image: {
-            link: 'img/project3.png',
-            alt: 'Uber Navigation screenshot',
-        },
-        liveSource: '#',
-        sourceCode: '#',
-    },
+  id: 0,
+  name: 'Tonic',
+  companyName: 'CANOPY',
+  position: 'Back End Dev',
+  year: 2015,
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  technologies: ['html', 'css', 'javascript'],
+  image: {
+    link: 'img/project0.png',
+    alt: 'Tonic screenshot',
+  },
+  liveSource: '#',
+  sourceCode: '#',
+},
+{
+  id: 1,
+  name: 'Multi-Post Stories',
+  companyName: 'FACEEBOOK',
+  position: 'Full Stack Dev',
+  year: 2015,
+  description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+  technologies: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  image: {
+    link: 'img/project1.png',
+    alt: 'Multi-Post Stories screenshot',
+  },
+  liveSource: '#',
+  sourceCode: '#',
+},
+{
+  id: 2,
+  name: 'Facebook 360',
+  companyName: 'FACEEBOOK',
+  position: 'Full Stack Dev',
+  year: 2015,
+  description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+  technologies: ['html', 'css', 'javascript'],
+  image: {
+    link: 'img/project2.svg',
+    alt: 'Facebook 360screenshot',
+  },
+  liveSource: '#',
+  sourceCode: '#',
+},
+{
+  id: 3,
+  name: 'Uber Navigation',
+  companyName: 'Uber',
+  position: 'Lead Developer',
+  year: 2018,
+  description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+  technologies: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  image: {
+    link: 'img/project3.png',
+    alt: 'Uber Navigation screenshot',
+  },
+  liveSource: '#',
+  sourceCode: '#',
+},
 ];
 
 const modalDetails = document.createElement('div');
@@ -77,7 +77,7 @@ modalDetails.id = 'modal-details';
 modalDetails.className = 'project-details';
 
 function showDetails(projectIndex) {
-    const projectsCode = `
+  const projectsCode = `
   <div class="details-container">
   <span id="closeDetails"><i class="fas fa-times closeDetails"></i></span>
   <h2 class="project-title">${projects[projectIndex].name}</h2>
@@ -157,20 +157,17 @@ showButton.forEach((btn) => {
 });
 
 // get local storage data
-const btn_contact = document.querySelector
-  ('.submit');
-const subject = document.querySelector
-  ('.subject');
-const email = document.querySelector
-  ('.email');
-const message = document.querySelector
-  ('.message');
+// eslint-disable-next-line camelcase
+const btn_contact = document.querySelector('.submit');
+const subject = document.querySelector('.subject');
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
 
-btn_contact.onclick = function() {
+btn_contact.onclick = () => {
   const formInfo = {
     name: subject.value,
     email: email.value,
     message: message.value,
   };
   localStorage.setItem('formInfo', JSON.stringify(formInfo));
-}
+};
